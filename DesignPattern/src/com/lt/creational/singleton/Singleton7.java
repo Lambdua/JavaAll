a56cdb05fc06c76a356983903c5fc310b7681210
liangtao <lt547670718@gmail.com>
2021-01-24 14:06:24 +0800

    枚举单例的重写，去除旧demo

--- DesignPattern/src/com/lt/creational/singleton/Singleton7.java
+++ DesignPattern/src/com/lt/creational/singleton/Singleton7.java
@@ -5,6 +5,28 @@ package com.lt.creational.singleton;
  * @description 单例枚举实现
  * @Date 2021/1/24
  **/
-public enum Singleton7 {
-    INSTANCE;
+public class Singleton7 {
+    private Singleton7() {
+    }
+
+    /**
+     * 枚举类型是线程安全的，并且只会装载一次
+     */
+    private enum Singleton7Enum {
+        INSTANCE;
+        private final Singleton7 singleton7;
+
+        Singleton7Enum() {
+            this.singleton7 = new Singleton7();
+        }
+
+        //可以省略，外部类可以直接拿到内部枚举的属性
+        private Singleton7 getInstance(){
+            return singleton7;
+        }
+    }
+
+    public static Singleton7 getInstance() {
+        return Singleton7Enum.INSTANCE.getInstance();
+    }
 }
