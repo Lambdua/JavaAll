9b39de01499f6867d6f2c3206f1f3e08032e5186
liangtao <lt547670718@gmail.com>
2021-01-25 21:36:26 +0800

    背包的实现

--- algorithm/src/com/lt/base/Bag.java
+++ algorithm/src/com/lt/base/Bag.java
@@ -16,11 +16,35 @@ import java.util.Iterator;
  * 不重要。
  * @date 2021年01月22 14:06
  **/
-public abstract class Bag<Item extends Comparable<Item>>  {
+public class Bag<Item> implements Iterable<Item> {
+    private Node first;
 
-
-//    @Override
+    private class Node {
+        Item item;
+        Node next;
+    }
+    public void add(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+    @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new Iterator<Item>() {
+            private Node current = first;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public Item next() {
+                Item item = current.item;
+                current = current.next;
+                return item;
+            }
+        };
     }
 }
