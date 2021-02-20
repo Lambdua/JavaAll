b68c5ff64d1f749455f7130fef34cba00a18251d
liang tao <liangtao@agile-china.com>
2021-02-20 14:29:06 +0800

    链表的符号表实现以及测试用例

---
+++ algorithm/src/com/lt/base/symbol/SequentialSearchST.java
@@ -0,0 +1,85 @@
+package com.lt.base.symbol;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author liangtao
+ * @description 基于链表的符号表实现
+ * @date 2021年02月06 13:44
+ **/
+public class SequentialSearchST<K extends Comparable<K>, V> implements ST<K, V> {
+    /**
+     * 节点数量
+     */
+    private int N = 0;
+    /**
+     * 链表表头
+     */
+    private Node first;
+
+    //内部链表节点构造
+    private class Node {
+        private K key;
+        private V val;
+        private Node next;
+
+        public Node(K key, V val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    @Override
+    public void put(K key, V value) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                //命中旧节点，更新
+                x.val = value;
+                return;
+            }
+        }
+        // 未命中，新建结点
+        first = new Node(key, value, first);
+        N++;
+    }
+
+    @Override
+    public V get(K key) {
+        for (Node node = first; node != null; node = node.next) {
+            if (node.key.equals(key)) {
+                return node.val;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return N;
+    }
+
+    @Override
+    public Iterable<K> keys() {
+        return () -> new Iterator<K>() {
+            Node h = first;
+
+            @Override
+            public boolean hasNext() {
+                return h != null;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                K k = h.key;
+                h = h.next;
+                return k;
+            }
+        };
+    }
+}
--- algorithm/src/com/lt/test/BaseTest.java
+++ algorithm/src/com/lt/test/BaseTest.java
@@ -1,4 +1,4 @@
-package com.lt.base;
+package com.lt.test;
 
 
 import com.lt.base.pqueue.MaxPQ;
@@ -7,8 +7,11 @@ import com.lt.base.queue.QueueByLink;
 import com.lt.base.stack.Stack;
 import com.lt.base.stack.StackByArray;
 import com.lt.base.symbol.ST;
+import com.lt.base.symbol.SequentialSearchST;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+
+import java.io.IOException;
 
 /**
  * @author liangtao
@@ -16,13 +19,19 @@ import edu.princeton.cs.algs4.StdOut;
  * @date 2021年01月25 16:41
  **/
 public class BaseTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        stackTest();
 //        queueTest();
 //        maxPQTest();
-        stBehaviorTest();
+//        stBehaviorTest();
+//        stPerformanceTest(12);
+//        stBehaviorTest("tinyTale.txt",new SequentialSearchST<>());
+        stPerformanceTest(1, "tinyTale.txt", new SequentialSearchST<>());
     }
 
+    /**
+     * 队列测试
+     */
     public static void queueTest() {
         // 创建一个队列并操作字符串入列或出列
         Queue<String> q = new QueueByLink<>();
@@ -41,6 +50,9 @@ public class BaseTest {
 
     }
 
+    /**
+     * 栈测试
+     */
     public static void stackTest() {
         // 创建一个栈并根据StdIn中的指示压入或弹出字符串
         Stack<String> s = new StackByArray<>();
@@ -59,6 +71,9 @@ public class BaseTest {
         StdOut.println("(" + s.size() + " left on stack)");
     }
 
+    /**
+     * 优先队列测试
+     */
     public static void maxPQTest() {
         MaxPQ<Integer> pq = new MaxPQ<>();
         while (!StdIn.isEmpty()) {
@@ -75,40 +90,48 @@ public class BaseTest {
      * 符号表。我们假设所有的字符串都只有一个字母。一般我们会使用 "S E A R C H E X A  M P L E"。
      * 按照我们的约定，用例会将键 S 和 0，键 R 和 3 关联起来，等等。但对于重复的字母，如E则是和12 、A则是和8关联起来。
      * 即<String,Integet>的<k,v>形式，重复元素的put会被顶替掉
+     *
+     * @param sourceName 测试数据source目录下的名称
      */
-    public static void stBehaviorTest() {
-        //todo  实现后测试
-        ST<String, Integer> st = null;
-
+    public static void stBehaviorTest(String sourceName, ST<String, Integer> st) throws IOException {
+        initialSystemIn(sourceName);
         for (int i = 0; !StdIn.isEmpty(); i++) {
             String key = StdIn.readString();
             st.put(key, i);
         }
         //对于有序的符号表实现，元素的输出顺序是确定的,无序的符号表输出则是不确定的
-        for (String s : st.keys()) StdOut.println(s + " " + st.get(s));
+        for (String s : st.keys()) StdOut.print(s + "->" + st.get(s) + " ");
     }
 
     /**
      * 符号表的性能测试用例,用例会从标准输入中得到的一列字符串并记录每个（长度至少达到指定的
      * 阈值）字符串的出现次数，然后遍历所有键并找出出现频率最高的键。
-     * @param minLen 筛选的单词最小长度
+     *
+     * @param minLen     筛选的单词最小长度
+     * @param sourceName 测试数据source目录下的名称
      */
-    public static void stPerformanceTest(int minLen){
-        ST<String,Integer> st=null;
-        while (!StdIn.isEmpty()){
-            String word= StdIn.readString();
-            if (word.length()<minLen) continue;
-            if (st.contains(word))  st.put(word,st.get(word)+1);
-            else st.put(word,1);
+    public static void stPerformanceTest(int minLen, String sourceName, ST<String, Integer> st) throws IOException {
+        initialSystemIn(sourceName);
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (word.length() < minLen) continue;
+            if (st.contains(word)) st.put(word, st.get(word) + 1);
+            else st.put(word, 1);
         }
 
-        String maxKey=" ";
-        st.put(maxKey,0);
+        String maxKey = " ";
+        st.put(maxKey, 0);
         for (String word : st.keys()) {
-            if (st.get(word)>st.get(maxKey)){
-                maxKey=word;
+            if (st.get(word) > st.get(maxKey)) {
+                maxKey = word;
             }
         }
-        StdOut.println("max-->"+st.get(maxKey));
+        StdOut.println(maxKey + "-->" + st.get(maxKey));
+    }
+
+    private static void initialSystemIn(String sourceName) throws IOException {
+        System.in.close();
+        //初始化system.in
+        System.setIn(BaseTest.class.getClassLoader().getResourceAsStream(sourceName));
     }
 }
